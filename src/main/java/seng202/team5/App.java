25fe7784df92a9e805739dc80e3a8082473fb7da
Oliver Cranshaw <oli@cranshaw.co.nz>
2020-08-08 12:27:01 +1200

    Added test button

--- src/main/java/seng202/team5/App.java
+++ src/main/java/seng202/team5/App.java
@@ -2,6 +2,7 @@ package seng202.team5;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -18,7 +19,15 @@ public class App extends Application {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Button button = new Button();
+        button.setText("CLICK ME");
+
+        StackPane layout = new StackPane();
+
+        layout.getChildren().add(l);
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
