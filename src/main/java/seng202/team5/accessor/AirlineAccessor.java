1881154103dd696c1c415009d57781e4182250a2
Inga Tokarenko <ito16@uclive.ac.nz>
2020-08-11 16:04:43 +1200

    try2

--- src/main/java/seng202/team5/accessor/AirlineAccessor.java
+++ src/main/java/seng202/team5/accessor/AirlineAccessor.java
@@ -1,4 +1,10 @@
 package seng202.team5.accessor;
 
+import java.util.ArrayList;
+
 public class AirlineAccessor implements Accessor {
+
+    public int save(ArrayList data) {
+        return -1;
+    }
 }
--- src/main/java/seng202/team5/accessor/RouteAccessor.java
+++ src/main/java/seng202/team5/accessor/RouteAccessor.java
@@ -1,4 +1,11 @@
 package seng202.team5.accessor;
 
+import java.util.ArrayList;
+
 public class RouteAccessor implements Accessor {
+
+    public int save(ArrayList data) {
+        return -1;
+    }
+
 }
