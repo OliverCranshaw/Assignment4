83c7c9e472e8854a5f40aabc9da63a3f0b3341d2
Oliver Cranshaw <oli@cranshaw.co.nz>
2020-08-14 21:43:36 +1200

    Adding navigation between tabs and screens

--- src/main/java/seng202/team5/App.java
+++ src/main/java/seng202/team5/App.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 
@@ -18,14 +19,49 @@ import java.io.IOException;
  */
 public class App extends Application {
 
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage mainStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        stage.setTitle("Flight Data Manager");
-        stage.setScene(new Scene(root, 1280, 720));
-        stage.show();
+        mainStage.setTitle("Flight Data Manager");
+        mainStage.setScene(new Scene(root, 1280, 720));
+        mainStage.show();
     }
 
+    public static void launchAddAirportStage() throws IOException {
+
+        Stage addAirportStage = new Stage();
+
+        Parent root = FXMLLoader.load(App.class.getResource("add_airport.fxml"));
+        addAirportStage.setTitle("Add Airport");
+        addAirportStage.setScene(new Scene(root, 1280, 720));
+
+        addAirportStage.show();
+
+    }
+
+
+    public static void launchAddAirlineStage() throws IOException {
+
+        Stage addAirlineStage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("add_airline.fxml"));
+        addAirlineStage.setTitle("Add Airport");
+        addAirlineStage.setScene(new Scene(root, 1280, 720));
+
+        addAirlineStage.show();
+    }
+
+
+
+    public static void launchAddRouteStage() throws IOException {
+
+        Stage addRouteStage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("add_route.fxml"));
+        addRouteStage.setTitle("Add Airport");
+        addRouteStage.setScene(new Scene(root, 1280, 720));
+
+        addRouteStage.show();
+    }
 
 
     public static void main(String[] args) {
