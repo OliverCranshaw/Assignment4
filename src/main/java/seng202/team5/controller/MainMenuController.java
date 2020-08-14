5fb8a6ff8aea0b729da901613f5b12d87e01e083
Nathan Smithies <nathensmithies@gmail.com>
2020-08-14 18:09:24 +1200

    Added controller for main menu

---
+++ src/main/java/seng202/team5/controller/MainMenuController.java
@@ -0,0 +1,18 @@
+package seng202.team5.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+    @FXML
+    public void onAddAirportPressed() {
+    }
+
+    @FXML
+    public void onAddAirlinePressed() {
+    }
+
+    @FXML
+    public void onAddRoutePressed() {
+    }
+}
