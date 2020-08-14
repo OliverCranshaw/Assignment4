cff0c1a0e089efa66634c67ff51fe736592dd519
Nathan Smithies <nathensmithies@gmail.com>
2020-08-14 18:44:08 +1200

    Hooked up "add X" menus to the main menu

--- src/main/java/seng202/team5/controller/MainMenuController.java
+++ src/main/java/seng202/team5/controller/MainMenuController.java
@@ -2,17 +2,47 @@ package seng202.team5.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import seng202.team5.App;
+
+import java.io.IOException;
 
 public class MainMenuController {
     @FXML
-    public void onAddAirportPressed() {
+    public void onAddAirportPressed(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("add_airport.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Add Airport");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow() );
+        stage.show();
     }
 
     @FXML
-    public void onAddAirlinePressed() {
+    public void onAddAirlinePressed(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("add_airline.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Add Airline");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow() );
+        stage.show();
     }
 
     @FXML
-    public void onAddRoutePressed() {
+    public void onAddRoutePressed(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("add_route.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Add Route");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow() );
+        stage.show();
     }
 }
