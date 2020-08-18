a87d8aeda0fd0aa9ce0fe2ca36a75cdf235ee915
Nathan Smithies <nathensmithies@gmail.com>
2020-08-18 16:02:46 +1200

    Added test for AirlineService Doesn't currently pass

--- src/test/java/service/AirlineServiceTest.java
+++ src/test/java/service/AirlineServiceTest.java
@@ -1,4 +1,66 @@
 package service;
 
-public class AirlineServiceTest {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import seng202.team5.database.DBConnection;
+import seng202.team5.database.DBTableInitializer;
+import seng202.team5.service.AirlineService;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import seng202.team5.database.DBInitializer;
+
+public class AirlineServiceTest extends TestCase {
+    private static final String dbFile = "test.db";
+
+
+    private AirlineService airlineService;
+
+    public AirlineServiceTest(String testName) { super(testName); }
+
+    public static Test suite() { return new TestSuite(AirportServiceTest.class); }
+
+
+    protected void setUp() {
+        System.out.println("Setup");
+        DBInitializer.createNewDatabase(dbFile);
+        DBTableInitializer.initializeTables(dbFile);
+
+        DBConnection.setDatabaseFile(new File(dbFile));
+        airlineService = new AirlineService();
+    }
+
+    protected void tearDown() throws SQLException {
+        System.out.println("Tear down");
+        Connection conn = DBConnection.getConnection();
+        conn.close();
+
+        new File(dbFile).delete();
+    }
+
+    public void testInitialState() throws SQLException {
+        ResultSet stuff = airlineService.getAirlines(null, null, null);
+        assertFalse(stuff.next());
+    }
+
+    public void testAddAirline() throws SQLException {
+        String name = "AirportName";
+        String alias = "AliasName";
+        String country = "CountryName";
+        String iata = "IAT";
+        String icao = "ICAO";
+        String callsign = "CallsignStuff";
+        String active = "Y";
+
+        int res = airlineService.saveAirline(name, alias, iata, icao, callsign, country, active);
+        assertTrue(res != -1);
+
+        ResultSet resultSet = airlineService.getAirlines(null, null, null);
+        assertTrue(resultSet.next());
+        assertFalse(resultSet.next());
+    }
 }
