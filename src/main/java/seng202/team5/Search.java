12ca9e792d10dd9f58e8202bf20b1d3fe509fee8
Inga Tokarenko <ito16@uclive.ac.nz>
2020-08-19 19:19:02 +1200

    search class created

---
+++ src/main/java/seng202/team5/Search.java
@@ -0,0 +1,16 @@
+package seng202.team5;
+
+import java.util.ArrayList;
+
+public class Search {
+
+    private static Search searchInstance;
+    private ArrayList<Object> searchData;
+
+    public static Search getInstance() {
+        return searchInstance;
+    }
+
+//    some other function idk
+
+}
