d6882b1baf1bb56e66768f71080a669b70004c13
Inga Tokarenko <ito16@uclive.ac.nz>
2020-10-02 01:23:37 +1300

    completed airport graph chart test

--- src/test/java/seng202/team5/graph/AirlineGraphChartTest.java
+++ src/test/java/seng202/team5/graph/AirlineGraphChartTest.java
@@ -1,4 +1,34 @@
 package seng202.team5.graph;
 
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AirlineGraphChartTest {
+
+    private ObservableList<PieChart.Data> countryCountData;
+
+    @Before
+    public void setUp() {
+
+    }
+
+
+    @Test
+    public void testBuildChart() {
+
+    }
+
+
+    @Test
+    public void testAirlineCountryChart() {
+
+    }
+
+
+    @Test
+    public void testSortChartList() {
+
+    }
 }
